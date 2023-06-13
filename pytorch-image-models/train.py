f3579cafe066e5c28e60367a73da9fa60d439c72
crrrr30 <jonathancui03@gmail.com>
2023-06-13 08:14:25 -0400

    Update

--- pytorch-image-models/train.py
+++ pytorch-image-models/train.py
@@ -472,7 +472,7 @@ def main():
             model = NativeDDP(model, device_ids=[args.local_rank], broadcast_buffers=not args.no_ddp_bb)
         # NOTE: EMA model does not need to be wrapped by DDP
         
-    model = torch.compile(model)
+    model = torch.compile(model, mode="reduce-overhead")
 
     # setup learning rate schedule and starting epoch
     lr_scheduler, num_epochs = create_scheduler(args, optimizer)
