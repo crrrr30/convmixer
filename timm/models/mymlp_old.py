a2025e38612ecb45873eeb7a7b558be1093439c3
crrrr30 <jonathancui03@gmail.com>
2023-06-21 15:10:38 -0400

    Update

--- timm/models/mymlp_old.py
+++ timm/models/mymlp_old.py
@@ -17,188 +17,6 @@ from einops._torch_specific import allow_ops_in_compiled_graph  # requires einop
 allow_ops_in_compiled_graph()
 
 
-# the code is modified from https://github.com/d-li14/involution/blob/main/cls/mmcls/models/utils/involution_cuda.py
-from torch.autograd import Function
-import torch
-from torch.nn.modules.utils import _pair
-import torch.nn.functional as F
-import torch.nn as nn
-
-from collections import namedtuple
-import cupy
-from string import Template
-import math
-
-Stream = namedtuple('Stream', ['ptr'])
-
-
-def Dtype(t):
-    if isinstance(t, torch.cuda.FloatTensor):
-        return 'float'
-    elif isinstance(t, torch.cuda.DoubleTensor):
-        return 'double'
-
-
-@cupy._util.memoize(for_each_device=True)
-def load_kernel(kernel_name, code, **kwargs):
-    code = Template(code).substitute(**kwargs)
-    kernel_code = cupy.cuda.compile_with_cache(code)
-    return kernel_code.get_function(kernel_name)
-
-
-CUDA_NUM_THREADS = 1024
-
-kernel_loop = '''
-#define CUDA_KERNEL_LOOP(i, n)                        \
-  for (int i = blockIdx.x * blockDim.x + threadIdx.x; \
-      i < (n);                                       \
-      i += blockDim.x * gridDim.x)
-'''
-
-
-def GET_BLOCKS(N):
-    return (N + CUDA_NUM_THREADS - 1) // CUDA_NUM_THREADS
-
-
-_shift_kernel = kernel_loop + '''
-extern "C"
-__global__ void shift_forward_kernel(
-const ${Dtype}* bottom_data, ${Dtype}* top_data) {
-  CUDA_KERNEL_LOOP(index, ${nthreads}) {
-    const int n = index / ${channels} / ${height} / ${width};
-    const int c = (index / ${height} / ${width}) % ${channels};
-    const int h = (index / ${width}) % ${height};
-    const int w = index % ${width};
-
-    ${Dtype} value = 0;
-    const int s1 = c % ${shift} - ${shift} / 2;
-    const int s2 = (c / ${shift}) % ${shift} - ${shift} / 2;
-    const int h_prime = h + s1;
-    const int w_prime = w + s2;
-
-    if ((h_prime >= 0 && h_prime < ${height}) && (w_prime >= 0 && w_prime < ${width})) {
-        const int offset = ((n * ${channels} + c) * ${height} + h_prime) * ${width} + w_prime;
-        value = bottom_data[offset];
-    }
-
-    top_data[index] = value;
-  }
-}
-'''
-
-
-_shift_kernel_backward_grad_input = kernel_loop + '''
-extern "C"
-__global__ void shift_backward_grad_input_kernel(
-    const ${Dtype}* const top_diff, ${Dtype}* const bottom_diff) {
-  CUDA_KERNEL_LOOP(index, ${nthreads}) {
-    const int n = index / ${channels} / ${height} / ${width};
-    const int c = (index / ${height} / ${width}) % ${channels};
-    const int h = (index / ${width}) % ${height};
-    const int w = index % ${width};
-    
-    ${Dtype} value = 0;
-    const int s1 = c % ${shift} - ${shift} / 2;
-    const int s2 = (c / ${shift}) % ${shift} - ${shift} / 2;
-    const int h_prime = h + s1;
-    const int w_prime = w + s2;
-
-    if ((h_prime >= 0 && h_prime < ${height}) && (w_prime >= 0 && w_prime < ${width})) {
-        const int offset = ((n * ${channels} + c) * ${height} + h_prime) * ${width} + w_prime;
-        value = top_diff[offset];
-    }
-    
-    bottom_diff[index] = value;
-  }
-}
-'''
-
-
-class _shift(Function):
-    @staticmethod
-    def forward(ctx, input, shift):
-        assert input.dim() == 4 and input.is_cuda
-        batch_size, channels, height, width = input.size()
-
-        output = input.new(batch_size, channels, height, width)
-        n = output.numel()
-
-        with torch.cuda.device_of(input):
-            f = load_kernel('shift_forward_kernel', _shift_kernel, Dtype=Dtype(input), nthreads=n,
-                num=batch_size, channels=channels, 
-                height=height, width=width,
-                shift=shift, group=int(math.ceil(channels/shift))
-            )
-
-            f(block=(CUDA_NUM_THREADS,1,1),
-              grid=(GET_BLOCKS(n),1,1),
-              args=[input.data_ptr(), output.data_ptr()],
-              stream=Stream(ptr=torch.cuda.current_stream().cuda_stream))
-
-        ctx.save_for_backward(input)
-        ctx.shift = shift
-        return output
-    
-    @staticmethod
-    def backward(ctx, grad_output):
-        assert grad_output.is_cuda
-        if not grad_output.is_contiguous():
-            grad_output = grad_output.contiguous()
-        input = ctx.saved_tensors[0]
-        shift = ctx.shift
-        batch_size, channels, height, width = input.size()
-
-        grad_input = None
-
-        opt = dict(Dtype=Dtype(grad_output),
-            num=batch_size, channels=channels,
-            height=height, width=width,
-            shift=shift, group=int(math.ceil(channels/shift))
-        )
-
-        with torch.cuda.device_of(input):
-            if ctx.needs_input_grad[0]:
-                grad_input = input.new(input.size())
-
-                n = grad_input.numel()
-                opt['nthreads'] = n
-
-                f = load_kernel('shift_backward_grad_input_kernel',
-                                _shift_kernel_backward_grad_input, **opt)
-                f(block=(CUDA_NUM_THREADS,1,1),
-                  grid=(GET_BLOCKS(n),1,1),
-                  args=[grad_output.data_ptr(), grad_input.data_ptr()],
-                  stream=Stream(ptr=torch.cuda.current_stream().cuda_stream))
-
-        return grad_input, None, None
- 
-
-def _shift_cuda(input, shift):
-    """ shift kernel
-    """
-    assert shift >= 3 and shift % 2 == 1
-
-    if input.is_cuda:
-        out = _shift.apply(input, shift)
-    else:
-        raise NotImplementedError
-    return out
-
-
-class Shift(nn.Module):
-    def __init__(self, kernel_size):
-        super().__init__()
-        self.kernel_size = kernel_size
-        assert kernel_size % 2 == 1
-
-    def forward(self, x):
-        if self.kernel_size == 1:
-            return x
-
-        out = _shift_cuda(x, self.kernel_size)
-        return out
-
-
 class Mlp(nn.Module):
     def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.):
         super().__init__()
@@ -246,8 +64,41 @@ class CycleFC(nn.Module):
         else:
             self.channel_mixer = None
 
-        self.shift = Shift(kernel_size)
-        self.linear = nn.Conv2d(in_channels, out_channels, 1, 1, 0, bias=bias)
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.groups = groups
+
+        self.weight = nn.Parameter(torch.empty(out_channels, in_channels // groups, 1, 1))  # kernel size == 1
+
+        if bias:
+            self.bias = nn.Parameter(torch.empty(out_channels))
+        else:
+            self.register_parameter('bias', None)
+        self.register_buffer('offset', self.gen_offset())
+
+        self.reset_parameters()
+
+    def reset_parameters(self) -> None:
+        init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+
+        if self.bias is not None:
+            fan_in, _ = init._calculate_fan_in_and_fan_out(self.weight)
+            bound = 1 / math.sqrt(fan_in)
+            init.uniform_(self.bias, -bound, bound)
+
+    def gen_offset(self):
+        """
+        offset (Tensor[batch_size, 2 * offset_groups * kernel_height * kernel_width,
+            out_height, out_width]): offsets to be applied for each position in the
+            convolution kernel.
+        """
+        # offset shape: (1, self.in_channels*2, 1, 1)
+        
+        k = self.kernel_size
+        # Note: Unary - takes precedence over binary //
+        return torch.Tensor([[i, j] for j in range(-(k//2), k//2+1) for i in range(-(k//2),k//2+1)] \
+            * (self.in_channels // k**2)).view(1, -1, 1, 1)
 
     def forward(self, input):
         """
@@ -256,7 +107,9 @@ class CycleFC(nn.Module):
         """
         if self.channel_mixer:
             input = self.channel_mixer(input)
-        return self.linear(self.shift(input))
+        B, C, H, W = input.size()
+        return deform_conv2d(input, self.offset.expand(B, -1, H, W), self.weight, self.bias, stride=1,
+                                padding=0, dilation=1)
 
 class MyMLPLayer(nn.Module):
     r""" Axial shift  
