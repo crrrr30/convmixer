d100cb5c9e57a42252b9eb882c483751df3e3f80
crrrr30 <jonathancui03@gmail.com>
2023-07-02 17:44:12 -0400

    Update

--- timm/models/lipscswinmlp.py
+++ timm/models/lipscswinmlp.py
@@ -376,28 +376,28 @@ class CSWinMLPTransformer(nn.Module):
 
 @register_model
 def CSWinMLP_64_12211_tiny_224(pretrained=False, **kwargs):
-    model = CSWinMLPTransformer(patch_size=4, embed_dim=64, depth=[4,3,8,3],
+    model = CSWinMLPTransformer(patch_size=4, embed_dim=64, depth=[4,3,8,3], d=2,
         split_size=[1,2,7,7], num_heads=[1,1,1,1], mlp_ratio=4.)
     model.default_cfg = default_cfgs['cswinmlp_224']
     return model
 
 @register_model
 def CSWinMLP_64_24322_small_224(pretrained=False, **kwargs):
-    model = CSWinMLPTransformer(patch_size=4, embed_dim=64, depth=[2,4,32,2],
+    model = CSWinMLPTransformer(patch_size=4, embed_dim=64, depth=[2,4,32,2], d=4,
         split_size=[1,2,7,7], num_heads=[1,1,1,1], mlp_ratio=4.)
     model.default_cfg = default_cfgs['cswinmlp_224']
     return model
 
 @register_model
 def CSWinMLP_96_24322_base_224(pretrained=False, **kwargs):
-    model = CSWinMLPTransformer(patch_size=4, embed_dim=96, depth=[2,4,32,2],
+    model = CSWinMLPTransformer(patch_size=4, embed_dim=96, depth=[2,4,32,2], d=4,
         split_size=[1,2,7,7], num_heads=[1,1,1,1], mlp_ratio=4.)
     model.default_cfg = default_cfgs['cswinmlp_224']
     return model
 
 @register_model
 def CSWinMLP_144_24322_large_224(pretrained=False, **kwargs):
-    model = CSWinMLPTransformer(patch_size=4, embed_dim=144, depth=[2,4,32,2],
+    model = CSWinMLPTransformer(patch_size=4, embed_dim=144, depth=[2,4,32,2], d=8,
         split_size=[1,2,7,7], num_heads=[1,1,1,1], mlp_ratio=4.)
     model.default_cfg = default_cfgs['cswinmlp_224']
     return model
@@ -406,14 +406,14 @@ def CSWinMLP_144_24322_large_224(pretrained=False, **kwargs):
 
 @register_model
 def CSWinMLP_96_24322_base_384(pretrained=False, **kwargs):
-    model = CSWinMLPTransformer(patch_size=4, embed_dim=96, depth=[2,4,32,2],
+    model = CSWinMLPTransformer(patch_size=4, embed_dim=96, depth=[2,4,32,2], d=8,
         split_size=[1,2,12,12], num_heads=[4,4,4,8], mlp_ratio=4.)
     model.default_cfg = default_cfgs['cswinmlp_384']
     return model
 
 @register_model
 def CSWinMLP_144_24322_large_384(pretrained=False, **kwargs):
-    model = CSWinMLPTransformer(patch_size=4, embed_dim=144, depth=[2,4,32,2],
+    model = CSWinMLPTransformer(patch_size=4, embed_dim=144, depth=[2,4,32,2], d=8,
         split_size=[4,2,12,12], num_heads=[4,4,8,8], mlp_ratio=4.)
     model.default_cfg = default_cfgs['cswinmlp_384']
     return model
