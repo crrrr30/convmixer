379ade86c6a58562d432e71b9ebd9efa96cc25ea
crrrr30 <jonathancui03@gmail.com>
2023-07-02 23:16:25 -0400

    Update

--- train.py
+++ train.py
@@ -330,8 +330,8 @@ group.add_argument('--log-interval', type=int, default=50, metavar='N',
                    help='how many batches to wait before logging training status')
 group.add_argument('--recovery-interval', type=int, default=0, metavar='N',
                    help='how many batches to wait before writing recovery checkpoint')
-group.add_argument('--checkpoint-hist', type=int, default=10, metavar='N',
-                   help='number of checkpoints to keep (default: 10)')
+group.add_argument('--checkpoint-hist', type=int, default=1000, metavar='N',
+                   help='number of checkpoints to keep (default: 1000)')
 group.add_argument('-j', '--workers', type=int, default=4, metavar='N',
                    help='how many training processes to use (default: 4)')
 group.add_argument('--save-images', action='store_true', default=False,
